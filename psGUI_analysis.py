94a6f46c15a66ade10ef3e736058eaae3c4b0755
louzoul <65058635+louzoul@users.noreply.github.com>
2020-08-15 22:18:55 +0900

    200815

--- psGUI_analysis.py
+++ psGUI_analysis.py
@@ -18,77 +18,96 @@ matplotlib.use("TkAgg")
 def draw_figure(canvas, figure):
     figure_canvas_agg = FigureCanvasTkAgg(figure, canvas)
     figure_canvas_agg.draw()
-    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=10)
+    figure_canvas_agg.get_tk_widget().pack(side="top", fill="both", expand=True)
     figure_canvas_agg.get_tk_widget().grid(column=1,row=0)
     return figure_canvas_agg
 
 def set_plt(event):
-    if sub_values["logx"] == True:
+    if values_plot["logx"] == True:
         ax.set_xscale("log")
-    if sub_values["logy"] == True:
+    if values_plot["logy"] == True:
         ax.set_yscale("log")
-    if sub_values["rangex"] == True:
+    if values_plot["rangex"] == True:
         try:
-            ax.set_xlim(float(sub_values["xmin"]),float(sub_values["xmax"]))
+            ax.set_xlim(float(values_plot["xmin"]),float(values_plot["xmax"]))
         except ValueError:
             sg.popup_error("Error : Please input a numerical values")
             return
-    if sub_values["rangey"] == True:
+    if values_plot["rangey"] == True:
         try:
-            ax.set_ylim(float(sub_values["ymin"]),float(sub_values["ymax"]))
+            ax.set_ylim(float(values_plot["ymin"]),float(values_plot["ymax"]))
         except ValueError:
             sg.popup_error("Error : Please input a numerical values")
             return
-    ax.set_xlabel(values["labelx"])
-    ax.set_ylabel(values["labely"])
-    # ax.legend(loc="best",ncol=2)
+    ax.set_xlabel(values_plot["labelx"])
+    ax.set_ylabel(values_plot["labely"])
     ax.grid()
 
     try:
-        ax.plot(prepro_data[:,0],prepro_data[:,1],marker=sub_values["marker"],linestyle=sub_values["linestyle"])
+        if values_prepro["multiplot"]:
+            ax.plot(prepro_data[:,0],prepro_data[:,1:],marker=values_plot["marker"],linestyle=values_plot["linestyle"])
+        else:
+            ax.plot(prepro_data[:,0],prepro_data[:,1],marker=values_plot["marker"],linestyle=values_plot["linestyle"])            
     except NameError:
         sg.popup_error("Error : Please load a datafile")
         return
+
+    if values_plot["legend"] == True:
+        ax.legend(loc="best",ncol=2)
     
     return
 
 def prepro(event):
-    prepro_data = np.stack([data[:,int(values["Data-X"])],data[:,int(values["Data-Y"])]],1)
+    X = np.linspace(0,len(data),len(data))
+    if values_prepro["datapoints"] and values_prepro["multiplot"]:
+        prepro_data = np.vstack([X.T,np.delete(data,int(values_prepro["Data-X"]),1).T]).T
+    elif not values_prepro["datapoints"] and values_prepro["multiplot"]:
+        prepro_data = np.vstack([data[:,int(values_prepro["Data-X"])].T,np.delete(data,int(values_prepro["Data-X"]),1).T]).T
+    elif values_prepro["datapoints"] and not values_prepro["multiplot"]:
+        prepro_data = np.stack([X,data[:,int(values_prepro["Data-Y"])]],1)
+    elif not values_prepro["datapoints"] and not values_prepro["multiplot"]:
+        prepro_data = np.stack([data[:,int(values_prepro["Data-X"])],data[:,int(values_prepro["Data-Y"])]],1)
     return prepro_data
 
 def load_file(datafile):
     data = np.loadtxt(datafile)
-    window["matrix"].Update(data.shape)
+    window_prepro["row"].Update(int(data.size/len(data)))
+    window_prepro["column"].Update(len(data))
     return data
 
 # ------------------------------- Beginning of GUI CODE -------------------------------
 
+style_window = {"finalize":True, "element_justification":"left", "font":"Helvetica 18"}
+
 layout_main = f_layout("Main")
-window = sg.Window("Main Window", layout_main, finalize=True, element_justification="center", font="Helvetica 18")
+window = sg.Window("Main Window", layout_main, **style_window)
 
 layout_plot = f_layout("Plot")
-window_plot = sg.Window("Plot Window", layout_plot, finalize=True, element_justification="center", font="Helvetica 18")
+window_plot = sg.Window("Plot Window", layout_plot, **style_window)
 window_plot_active = True
 
+layout_prepro = f_layout("Prepro")
+window_prepro = sg.Window("Preprocessing Window", layout_prepro, **style_window)
+window_prepro_active = True
+
 fig = matplotlib.figure.Figure(figsize=(6,4), dpi=100)
+fig.subplots_adjust(bottom=0.2, right=0.9, top=0.9, left=0.2)
 fig_canvas_agg = draw_figure(window["-CANVAS-"].TKCanvas, fig)
 ax = fig.add_subplot(111)
 
 # イベントループ
 while True:
 
-    event,values = window.read(timeout=10)
+    event,values = window.read(timeout=100)
+
+    if values["loadfile"]:
+        datafile = values["loadfile"]
+        data = load_file(datafile)
     
     if event == None or event == "exit":
         window_plot.close()
+        window_prepro.close()
         break
-    
-    elif event == "load":
-        try:
-            datafile = sg.popup_get_file("Please select a datafile")
-            data = load_file(datafile)
-        except (OSError,ValueError,TypeError):
-            sg.popup_error("Error : Please select a datafile")
             
     elif event == "draw":
         ax.cla()
@@ -99,29 +118,37 @@ while True:
         try:           
             set_plt(event)
         except TypeError:
-            sub_values=[]
+            values_plot=[]
         fig_canvas_agg.draw()
-        
-    elif not window_plot_active and event == "popup":
-        window_plot_active = True
-        layout_plot = f_layout("Plot")
-        window_plot = sg.Window("Plot Window", layout_plot, finalize=True, element_justification="center", font="Helvetica 18")
-        
-    elif event == "save":
-        try:
-            file = sg.popup_get_file("Please select a savefile", save_as=True)
-            fig_canvas_agg.print_png(file)
-        except (FileNotFoundError,ValueError):
-            sg.popup_error("Error : Please select a savefile")
-            
     elif event == "clr":
         ax.cla()
         fig_canvas_agg.draw()
     
+        
+    elif not window_plot_active and event == "win_plot":
+        window_plot_active = True
+        layout_plot = f_layout("Plot")
+        window_plot = sg.Window("Plot Window", layout_plot, **style_window)
+    elif not window_prepro_active and event == "win_prepro":
+        layout_prepro = f_layout("Prepro")
+        window_prepro = sg.Window("Preprocessing Window", layout_prepro, **style_window)
+        window_prepro_active = True
+        
+    elif event == "save":
+        try:
+            fig_canvas_agg.print_png(values["savefile"])
+        except (FileNotFoundError,ValueError):
+            sg.popup_error("Error : Please select a savefile")
+            
     if window_plot_active:
-        sub_event,sub_values = window_plot.read(timeout=10)
-        if sub_event == None:
+        event_plot,values_plot = window_plot.read(timeout=100)
+        if event_plot == None:
             window_plot_active = False
             window_plot.close()
-            
+    if window_prepro_active:
+        event_prepro,values_prepro = window_prepro.read(timeout=100)
+        if event_prepro == None:
+            window_prepro_active = False
+            window_prepro.close()
+
 window.close()
--- psGUI_layout.py
+++ psGUI_layout.py
@@ -9,49 +9,55 @@ import PySimpleGUI as sg
 itm_marker = ["None",".",",","o","^"]
 itm_linestyle = ["None","solid","dashed","dashdot","dotted"]
 
-font_frame = {"font":("Helvetica",15)}
-font_check = {"default":False, "font":("Helvetica",15)}
-font_text = {"font":("Helvetica",15), "size":(7,1)}
-font_output = {"font":("Helvetica",15), "size":(7,1)}
-
+font_Frame = {"font":("Helvetica",16)}
+font_Checkbox = {"default":False, "font":("Helvetica",14)}
+font_InputText = {"font":("Helvetica",14), "size":(7,1)}
+font_Text = {"font":("Helvetica",14),"justification":"left"}
+font_Button = {"font":("Helvetica",14)}
+font_Output = {"font":("Helvetica",14), "size":(8,2)}
+font_InputCombo = {"font":("Helvetica",14)}
+font_Input = {"font":("Helvetica",13)}
 
 def f_layout(window):
 
     if window == "Main":
+        layout = [
+        [sg.Frame("LoadFile", **font_Frame, layout=[
+            [sg.Input(**font_Input),sg.FileBrowse(**font_Button, key="loadfile")]])],
+        [sg.Frame("Canvas", **font_Frame, layout=[
+            [sg.Canvas(key="-CANVAS-")]])],
+        [sg.Frame("SaveCanvas", **font_Frame, layout=[
+            [sg.Input(**font_Input),sg.FileSaveAs(**font_Button, key="savefile"), sg.OK(**font_Button, key="save")]])],
+        [sg.Frame("Button", **font_Frame, layout=[
+            [sg.Button("Draw", **font_Button, key="draw"),
+             sg.Button("Clear", **font_Button, key="clr"),
+             sg.Button("Setting Data", **font_Button, key="win_prepro"),
+             sg.Button("Setting Plot", **font_Button, key="win_plot"),
+             sg.Button("Exit", **font_Button, key="exit")]])]]
 
-        frame_canvas = sg.Frame("Canvas", **font_frame, layout=[
-                        [sg.Canvas(key="-CANVAS-")]])
-        
-        frame_mole = sg.Frame("Mole", **font_frame, layout=[
-                        [sg.Checkbox("ALL Check", **font_check, enable_events=True, key="check_all")],
-                        [sg.Output(**font_output, key="matrix")],
-                        [sg.Checkbox("Data-X", **font_check, key="check_12CO2"),sg.InputText(default_text = "0", **font_text, key = "Data-X")],
-                        [sg.Checkbox("Data-Y", **font_check, key="check_13CO2"),sg.InputText(default_text = "1", **font_text, key = "Data-Y")]])
-        
-        frame_label = sg.Frame("Label", **font_frame, layout=[
-                [sg.Text("Label X", **font_frame),sg.InputText(default_text = "X-axis", **font_text, key = "labelx",)],
-                [sg.Text("Label Y", **font_frame),sg.InputText(default_text = "Y-axis", **font_text, key = "labely",)]])
-        
-        frame_button = [sg.Frame("Button", **font_frame, layout=[
-                    [sg.Button("Load",key="load"),
-                    sg.Button("Draw",key="draw"),
-                    sg.Button("Popup",key="popup"),
-                    sg.Button("Clear",key="clr"),
-                    sg.Button("Save",key="save"),
-                    sg.Button("Exit",key="exit")]])]
-
-        layout = [[frame_mole,frame_canvas],[frame_label],frame_button]
+    elif window == "Prepro":
+        layout = [
+        [sg.Text("Row", **font_Text), sg.Output(**font_Output, key="row"),
+         sg.Text("Column", **font_Text), sg.Output(**font_Output, key="column")],
+        [sg.Checkbox("The number of data points use as X-Axis", **font_Checkbox, key="datapoints")],
+        [sg.Checkbox("Multiplot using the all data", **font_Checkbox, key="multiplot")],
+        [sg.Text("Data-X", **font_Text), sg.InputText(default_text = "0", **font_InputText, key = "Data-X"),
+         sg.Text("Data-Y", **font_Text), sg.InputText(default_text = "1", **font_InputText, key = "Data-Y")]]
 
     elif window == "Plot":
-        layout = [[sg.Text("Marker", **font_frame), sg.InputCombo(itm_marker, size=(10,len(itm_marker)), default_value="None", key="marker")],
-        [sg.Text("Linestyle", **font_frame), sg.InputCombo(itm_linestyle, size=(10,len(itm_linestyle)), default_value="solid", key="linestyle")],
-        [sg.Checkbox("ManualRange X", **font_check, key="rangex"),
-         sg.Checkbox("ManualRange Y", **font_check, key="rangey")],
-        [sg.Checkbox("LogScale X", **font_check, key="logx"),
-         sg.Checkbox("LogScale Y", **font_check, key="logy")],
-        [sg.Text("Xmin", **font_frame),sg.InputText(default_text = "0", **font_text, key = "xmin",),
-         sg.Text("Xmax", **font_frame),sg.InputText(default_text = "10", **font_text, key = "xmax",)],
-        [sg.Text("Ymin", **font_frame),sg.InputText(default_text = "-1.0", **font_text, key = "ymin",),
-         sg.Text("Ymax", **font_frame),sg.InputText(default_text = "1.0", **font_text, key = "ymax",)]]
+        layout = [
+        [sg.Text("Marker", **font_Text), sg.InputCombo(itm_marker, **font_InputCombo, size=(10,len(itm_marker)), default_value="None", key="marker"),
+         sg.Text("Linestyle", **font_Text), sg.InputCombo(itm_linestyle, **font_InputCombo, size=(10,len(itm_linestyle)), default_value="solid", key="linestyle")],
+        [sg.Checkbox("LogScale X", **font_Checkbox, key="logx"),
+         sg.Checkbox("LogScale Y", **font_Checkbox, key="logy")],
+        [sg.Checkbox("ManualRange X", **font_Checkbox, key="rangex"),
+         sg.Checkbox("ManualRange Y", **font_Checkbox, key="rangey")],
+        [sg.Text("Xmin", **font_Text),sg.InputText(default_text = "0", **font_InputText, key = "xmin",),
+         sg.Text("Xmax", **font_Text),sg.InputText(default_text = "10", **font_InputText, key = "xmax",)],
+        [sg.Text("Ymin", **font_Text),sg.InputText(default_text = "-1.0", **font_InputText, key = "ymin",),
+         sg.Text("Ymax", **font_Text),sg.InputText(default_text = "1.0", **font_InputText, key = "ymax",)],
+        [sg.Checkbox("Legend", **font_Checkbox, key="legend")],
+        [sg.Text("Label X", **font_Text),sg.InputText(default_text = "X-axis", **font_InputText, key = "labelx",),
+         sg.Text("Label Y", **font_Text),sg.InputText(default_text = "Y-axis", **font_InputText, key = "labely",)]]
     
     return layout
